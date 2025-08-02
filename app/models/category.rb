403fc390d81c69178f1badd62aadc6bb8eba6178
Marissa Abao <mabao@rrc.ca>
2025-08-02 10:41:09 -0500

    Add validations to Category model for name presence and uniqueness (4.2.1)

--- app/models/category.rb
+++ app/models/category.rb
@@ -1,15 +1,22 @@
 class Category < ApplicationRecord
+  # ----------------------------
+  # Associations
+  # ----------------------------
   has_many :product_categories, dependent: :destroy
   has_many :products, through: :product_categories
 
+  # ----------------------------
+  # Validations
+  # ----------------------------
   validates :name, presence: true, uniqueness: true
 
-  # Allowlist searchable associations
+  # ----------------------------
+  # Ransack Allowlist
+  # ----------------------------
   def self.ransackable_associations(auth_object = nil)
     ["product_categories", "products"]
   end
 
-  # Allowlist searchable attributes
   def self.ransackable_attributes(auth_object = nil)
     ["id", "name", "created_at", "updated_at"]
   end
