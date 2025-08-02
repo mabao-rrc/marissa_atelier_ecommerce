603ffc900f066ffeb50102923b523fbd01e9689d
Marissa Abao <mabao@rrc.ca>
2025-08-02 12:52:00 -0500

    Add presence validations to ProductCategory join model

--- app/models/product_category.rb
+++ app/models/product_category.rb
@@ -1,8 +1,19 @@
 class ProductCategory < ApplicationRecord
+  # ----------------------------
+  # Associations
+  # ----------------------------
   belongs_to :product
   belongs_to :category
 
-  # Allowlist searchable attributes for ActiveAdmin/Ransack
+  # ----------------------------
+  # Validations
+  # ----------------------------
+  # Ensure both foreign keys are present
+  validates :product_id, :category_id, presence: true
+
+  # ----------------------------
+  # Ransack Allowlist for ActiveAdmin Search
+  # ----------------------------
   def self.ransackable_attributes(auth_object = nil)
     ["id", "product_id", "category_id", "created_at", "updated_at"]
   end
