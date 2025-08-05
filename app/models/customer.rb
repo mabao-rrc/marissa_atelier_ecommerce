702c0a408caec467f3c9c4417eb6e9fab7a44481
Marissa Abao <mabao@rrc.ca>
2025-08-05 00:21:48 -0500

    Feature 3.1.4: Added Devise authentication for customers with styled sign-up and login pages

--- app/models/customer.rb
+++ app/models/customer.rb
@@ -1,4 +1,8 @@
 class Customer < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
   validates :first_name, :last_name, :email, :phone, :address, presence: true
   validates :email, format: { with: URI::MailTo::EMAIL_REGEXP }
   validates :phone, format: { with: /\A[\d\-\+\(\) ]+\z/, message: "only allows valid phone numbers" }
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :customers
   get "categories/show"
   get "storefront/index"
   get "products/index"
